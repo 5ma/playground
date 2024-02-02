244f8b065832972689db8acbd3a0b1784c8b0307
5ma <51451702+5ma@users.noreply.github.com>
2024-02-02 10:32:51 +0900

    Initial commit

---
+++ src/scripts/main.ts
@@ -0,0 +1,35 @@
+import collapse from '@alpinejs/collapse';
+import focus from '@alpinejs/focus';
+import ui from '@alpinejs/ui';
+import Alpine from 'alpinejs';
+
+async function loadAlpineModules() {
+  // enable code splitting
+  await import('./alpinejs');
+}
+
+async function main() {
+  // https://vitejs.dev/guide/env-and-mode.html#env-variables
+  if (import.meta.env.DEV) {
+    console.log({
+      MODE: import.meta.env.MODE,
+      BASE_URL: import.meta.env.BASE_URL,
+      PROD: import.meta.env.PROD,
+      DEV: import.meta.env.DEV,
+    });
+  }
+
+  Alpine.plugin(collapse);
+  Alpine.plugin(focus);
+  Alpine.plugin(ui);
+
+  await loadAlpineModules();
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+  (window as any).Alpine = Alpine;
+  Alpine.start();
+}
+
+main().catch((e) => {
+  throw e;
+});
